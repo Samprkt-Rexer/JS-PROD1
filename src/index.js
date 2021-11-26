f072cd8ab6da916f71ef734bd55220dbc86798cb
Depurk <2depurk@gmail.com>
2021-11-27 05:20:32 +0530

    create v1

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <div>
+  <>
   <App />
-  
-  <App />
-  </div>
+  </> 
 
-  ,
+  , 
   document.getElementById('root')
 );

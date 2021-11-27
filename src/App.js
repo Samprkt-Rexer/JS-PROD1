95050588208dd199d12c99d9328707d049613c7e
Shubham Raj <shubham14p3@gmail.com>
2021-11-28 02:08:09 +0530

    Trying Router DOM, pushing with errors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import './app.css';
-import Cse from './cs';
+import React from 'react'
+import { BrowserRouter as Link,Router, Route, Routes } from 'react-router-dom'
+import Cs from './cs';
 
-
-const App = () => {
- 
- 
+function App() {
   return (
-    
-      <Cse />
-      
-    
-  );
+    <div>
+       <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+            
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Routes>
+          <Route exact path="/" element={<Cs/>}/>
+          <Route exact path="/about" element={<Cs/>}/>
+         
+        </Routes>
+      </div>
+    </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App

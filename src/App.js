2a6c335cfac0e2b2951ecd64610bb0e73ca78472
Depurk <2depurk@gmail.com>
2021-11-27 08:00:42 +0530

    template added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './app.css';
+import PricingContent from './pricing';
 
 function App() {
   return (
     <div>
-      <div className="bgimg w3-display-container w3-animate-opacity w3-text-white">
-        <div className="w3-display-topleft w3-padding-large w3-xlarge">
-          Logo
-        </div>
-        <div className="w3-display-middle">
-          <h1 className="w3-jumbo w3-animate-top">COMING SOON</h1>
-          {/* <hr className="w3-border-grey" styles={"margin:auto;width:40%"}/> */}
-          <p className="w3-large w3-center">35 days left</p>
-        </div>
-        <div className="w3-display-bottomleft w3-padding-large">
-          Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" rel="noreferrer">w3.css</a>
-        </div>
-      </div>
-
+      <PricingContent />
     </div>
 
   );

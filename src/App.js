cbccedbf83662823ca652aaa5a997a5756af0c0c
Shubham Raj <shubham14p3@gmail.com>
2021-11-29 01:20:29 +0530

    final routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React from "react";
 // import ReactDom from 'react-dom'
 import "./app.css";
-import Cse from "./Pages/cs";
+
 import Navbar from "./Component/Navbar";
-import { Routes, Route } from 'react-router-dom';
+// import { } from 'react-router-dom';
 // import Pricing from "./Pages/pricing";
 
 const App = () => {
-  return (
-    <div id="root1">
-    <Navbar/>
-    <Routes>
-      <Route path='/welcome' element={<Cse />} />
-    </Routes>
-    hello
-    </div>
+  return (    
+    <Navbar/>   
   );
 };
 
 export default App;
-
-// const App = () => {
-//   return (
-//     <div>
-//     Hi
-//     <main>
-//       <Routes>
-//         <Route path='/welcome' element={<Cse />} />
-//         {/* <Route path='/products' element={<Products />} />
-//         <Route path='/products/:productId' element={<ProductDetail />} /> */}
-//       </Routes>
-//     </main>
 
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Cse from "../Pages/cs";
+import './Navbar.css';
 
-
-import { Link } from 'react-router-dom'
+import { Link, Routes, Route } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>
+    <div id="navbar" >
       <Link to="/">Home </Link>
       <Link to="/welcome">Wlcome </Link>
       <Link to="/contact">Contact Us</Link>
+      <Routes>
+      <Route path='/welcome' element={<Cse />} />
+    </Routes>
     </div>
   );
 }

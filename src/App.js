a032d69085bfc7792d715ea89fdf342a31feb167
Shubham Raj <shubham14p3@gmail.com>
2021-11-29 01:08:46 +0530

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import './app.css';
-import Cse from './cs';
-
+import React from "react";
+// import ReactDom from 'react-dom'
+import "./app.css";
+import Cse from "./Pages/cs";
+import Navbar from "./Component/Navbar";
+import { Routes, Route } from 'react-router-dom';
+// import Pricing from "./Pages/pricing";
 
 const App = () => {
- 
- 
   return (
-    
-      <Cse />
-      
-    
+    <div id="root1">
+    <Navbar/>
+    <Routes>
+      <Route path='/welcome' element={<Cse />} />
+    </Routes>
+    hello
+    </div>
   );
-}
+};
 
 export default App;
+
+// const App = () => {
+//   return (
+//     <div>
+//     Hi
+//     <main>
+//       <Routes>
+//         <Route path='/welcome' element={<Cse />} />
+//         {/* <Route path='/products' element={<Products />} />
+//         <Route path='/products/:productId' element={<ProductDetail />} /> */}
+//       </Routes>
+//     </main>
+
